705579e3a5e71f90d26532ace4fc339e7ffee628
Martijn Meijers <b.m.meijers@tudelft.nl>
2018-08-03 13:15:22 +0200

    Changed slightly continuous test script.

--- src/conttest.sh
+++ src/conttest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# find . -name '*.py' | entr ./do.sh
+# find . -name '*.py' | entr ./conttest.sh
 
 echo '* Picked up change (waiting for 2 secs)'
 sleep 2
 echo '* Running test suite'
 
 #python -m unittest discover -s . -p 'test_*.py'
-nosetests-2.7 test_*.py -v --with-coverage --cover-html
+nosetests-2.7 ./tests/test_*.py -v --with-coverage --cover-html
