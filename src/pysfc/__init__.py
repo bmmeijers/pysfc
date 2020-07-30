03f71dccfd6532a592b2075abcfae81e1e0e2a1f
Martijn Meijers <b.m.meijers@tudelft.nl>
2020-07-30 08:33:49 +0200

    Adding docstring to pysfc.__init__

---
+++ src/pysfc/__init__.py
@@ -0,0 +1,16 @@
+"""
+PySFC -- nD Space Filling Curves for indexing, clustering and querying
+         nD point clouds stored in PostgreSQL
+
+5----6    9----10
+|    |    |    |
+|    |    |    |
+4    7----8    11
+|              |
+|              |
+3----2    13---12
+     |    |
+     |    |
+0----1    14---15
+
+"""
